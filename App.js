01bb8e0a1447d6314de2ce3bebefca0f77d633c9
Cryptec <neupi92@web.de>
2020-05-15 15:12:55 +0200

    added svg

--- App.js
+++ App.js
@@ -9,13 +9,13 @@ export default function App() {
   return (
    
     <View style={styles.container}>
-      <Text style={{ fontSize: 23, marginTop: 35, textAlign: 'center', color: 'white' }}>Yummy!</Text>
+      <Text style={{ fontSize: 23, marginTop: 50, textAlign: 'center', color: 'white' }}>Yummy!</Text>
 
-      <View style={styles.svg}>
+      
       <SecuritySVG >
-              
+      style={styles.svg}   
            </SecuritySVG>
-      </View>
+      
 
       <TouchableOpacity
        
@@ -23,6 +23,7 @@ export default function App() {
           style={styles.button}>
           <Text style={{ fontSize: 23, textAlign: 'center', color: 'white' }}>Mahlzeit Anzeigen</Text>
         </TouchableOpacity>
+        
     </View>
   );
 }
@@ -38,10 +39,10 @@ const styles = StyleSheet.create({
   button: {
     marginHorizontal: 80,
     zIndex: 15,
-    marginTop: 300,
+    marginTop: 80,
   },
   svg: {
-    marginTop:0,
+    marginTop:80,
     marginHorizontal: 0,
     zIndex:20,
     
